024f7540928d7522da2b537ccbe6fb86b5f54e98
JVince <Vincewang10a7@gmail.com>
2024-04-21 20:49:13 +0900

    + Adjust the cooldown time of the shield ability + Create shield base class

---
+++ Source/LyraGame/Shield/Shield.cpp
@@ -0,0 +1,21 @@
+﻿#include "Shield.h"
+
+// Sets default values
+AShield::AShield()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AShield::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShield::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/LyraGame/Shield/Shield.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Shield.generated.h"
+
+UCLASS()
+class LYRAGAME_API AShield : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AShield();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
