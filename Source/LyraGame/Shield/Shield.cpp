7f422ce0ae9bb46df8eacb92399cd4480a9b0b59
JVince <Vincewang10a7@gmail.com>
2024-04-22 00:12:49 +0900

    Convert some function from BP to C++

--- Source/LyraGame/Shield/Shield.cpp
+++ Source/LyraGame/Shield/Shield.cpp
@@ -1,7 +1,22 @@
 ﻿#include "Shield.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogShield, Log, All);
+
+void AShield::OnShieldEndedDuration_Implementation()
+{
+	// Destroy Actor
+	this->Destroy();
+}
 
 // Sets default values
 AShield::AShield()
+	: ShieldInstigator{nullptr}
+	  , MeshComponent{nullptr}
+	  , ShieldInstigatorCharacter{nullptr}
+	  , CharacterCapsule{nullptr}
+	  , ShieldDuration(30.f)
 {
 	PrimaryActorTick.bCanEverTick = true;
 }
@@ -10,8 +25,19 @@ AShield::AShield()
 void AShield::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	ShieldInstigator = GetInstigator();
+	ShieldInstigatorCharacter = Cast<ACharacter>(ShieldInstigator);
+	MeshComponent = ShieldInstigatorCharacter->GetMesh();
+	CharacterCapsule = ShieldInstigatorCharacter->GetCapsuleComponent();
+
+	// Ignore actor when move myself
+	CharacterCapsule->IgnoreActorWhenMoving(this, true);
+	MeshComponent->IgnoreActorWhenMoving(ShieldInstigator, true);
+
+	// Set the event timer;
+	SetTimerForEvent();
 }
+
 
 // Called every frame
 void AShield::Tick(float DeltaTime)
@@ -19,3 +45,13 @@ void AShield::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void AShield::SetTimerForEvent()
+{
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	TimerManager.SetTimer(OnShieldEndedDurationTimerHandle, [this]
+	{
+		UE_LOG(LogShield, Log, TEXT("Shield ended"))
+		// OnShieldEndedDuration();
+		OnShieldEndedDurationTimerHandle.Invalidate();
+	}, ShieldDuration, false);
+}
--- Source/LyraGame/Shield/Shield.h
+++ Source/LyraGame/Shield/Shield.h
@@ -1,22 +1,58 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
+﻿#pragma once
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Shield.generated.h"
+
+class UCapsuleComponent;
 
 UCLASS()
 class LYRAGAME_API AShield : public AActor
 {
 	GENERATED_BODY()
 
+	// States //
+	// ====== //
+private:
+	UPROPERTY()
+	APawn* ShieldInstigator;
+
+	UPROPERTY()
+	USkeletalMeshComponent* MeshComponent;
+
+	UPROPERTY()
+	ACharacter* ShieldInstigatorCharacter;
+
+	UPROPERTY()
+	UCapsuleComponent* CharacterCapsule;
+
+	FTimerHandle OnShieldEndedDurationTimerHandle;
+
+protected:
+	// States //
+	// ====== //
+	UPROPERTY(EditAnywhere, Category=Shield, BlueprintReadWrite)
+	float ShieldDuration;
+
+	// Subroutines //
+	// =========== //
+	UFUNCTION(BlueprintNativeEvent)
+	void OnShieldEndedDuration();
+	
 public:
+	// Lifecycle //
+	// ========= //
 	AShield();
 
 protected:
+	// Lifecycle //
+	// ========= //
 	virtual void BeginPlay() override;
 
 public:
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	// Subroutines //
+	// =========== //
+	void SetTimerForEvent();
 };
